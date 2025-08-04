77db66c3f33cff7d84a6c2e9e0a18e83babd1029
Bojtronic <bojtronic@gmail.com>
2025-08-03 20:38:14 -0600

    Instalcion de angular material

--- TICpoint/src/app/app.ts
+++ TICpoint/src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './components/home/home';
+import { Contact } from './components/contact/contact';
+import {MatTabsModule} from '@angular/material/tabs';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home, Contact, MatTabsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
